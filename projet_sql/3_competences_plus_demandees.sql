48b7221873a78dd5a97b10ac1c1931fde8b9a3fb
JL Benoit <jean.benoit1993@gmail.com>
2025-10-15 14:45:09 +0200

    code de 3_competences_plus_demandees

--- projet_sql/3_competences_plus_demandees.sql
+++ projet_sql/3_competences_plus_demandees.sql
@@ -1,34 +1,18 @@
 /*
-
+Question : Quelles sont les compétences les plus demandées pour les analystes de données/data ?
+- Joindre les offres d'emploi à la table en utilisant un INNER JOIN, similaire à la requête 2.
+- Identifier les 5 compétences les plus demandées pour un analyste de données.
+- Se concentrer sur toutes les offres d'emploi.
+- Objectif : obtenir les 5 compétences les plus demandées sur le marché du travail, 
+fournissant ainsi des informations précieuses sur les compétences les plus recherchées par les employeurs.
 */
 
-WITH remote_job_skills AS (
-    SELECT 
-        skill_id,
-        COUNT(*) AS skill_count
-    FROM 
-        skills_job_dim AS skills_to_job
-    INNER JOIN 
-        job_postings_fact AS job_postings ON job_postings.job_id = skills_to_job.job_id
-    WHERE 
-        job_postings.job_work_from_home = TRUE 
-        AND job_postings.job_title_short = 'Data Analyst'
-    GROUP BY 
-        skill_id
-)
-SELECT 
-    skills.skill_id,
-    skills AS skill_name,
-    skill_count
+SELECT *
 FROM 
-    remote_job_skills
-INNER JOIN 
-    skills_dim AS skills ON skills.skill_id = remote_job_skills.skill_id
-ORDER BY
-    skill_count DESC
+    job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 LIMIT 5;
-
-
 
 /*
 
