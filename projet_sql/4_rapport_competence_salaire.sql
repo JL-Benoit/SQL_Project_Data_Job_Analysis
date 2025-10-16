38685bb5aeb16cd3753d766acc0fc0cc12ed91b0
JL Benoit <jean.benoit1993@gmail.com>
2025-10-16 10:55:06 +0200

    creation de 4_rapport_competence_salaire.sql

---
+++ projet_sql/4_rapport_competence_salaire.sql
@@ -0,0 +1,8 @@
+/*
+Question : Quelles sont les compétences les mieux rémunérées ?
+- Examiner le salaire moyen associé à chaque compétence pour les postes d’analyste de données/data.
+- Se concentrer sur les rôles avec des salaires spécifiés, quel que soit l’emplacement.
+- Objectif : Révèler comment différentes compétences influencent les niveaux de salaire des 
+analystes de données/data et aider à identifier les compétences les plus "rentables" à acquérir.
+*/
+
